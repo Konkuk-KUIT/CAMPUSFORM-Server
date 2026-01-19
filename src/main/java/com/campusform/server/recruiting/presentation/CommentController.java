cd3062ccd993b3a6d73a85ad25d0cfcacc08251d
jaewon <jaewon0077@naver.com>
2026-01-19 18:28:27 +0900

    [fix] Comment Controller 오류 수정

--- src/main/java/com/campusform/server/recruiting/presentation/CommentController.java
+++ src/main/java/com/campusform/server/recruiting/presentation/CommentController.java
@@ -2,6 +2,9 @@ package com.campusform.server.recruiting.presentation;
 
 import com.campusform.server.global.common.ApiResponse;
 import com.campusform.server.recruiting.application.CommentService;
+import com.campusform.server.recruiting.application.dto.request.CommentRequest;
+import com.campusform.server.recruiting.application.dto.response.CommentCreateResponse;
+import com.campusform.server.recruiting.application.dto.response.CommentUpdateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,38 +16,38 @@ public class CommentController {
 
     // 댓글 작성
     @PostMapping
-    public ApiResponse<CommentDto.CreateResponse> createComment(
+    public ApiResponse<CommentCreateResponse> createComment(
         @PathVariable Long projectId,
         @PathVariable Long applicantId,
         @RequestParam(defaultValue = "document") String stage,
-        @RequestBody CommentDto.Request request
+        @RequestBody CommentRequest request
     ){
         Long memberId = 1L; // 임시 하드코딩 (로그인 구현 시 교체 필요)
 
-        CommentDto.CreateResponse response = commentService.createComment(projectId, applicantId, memberId, request);
+        CommentCreateResponse response = commentService.createComment(applicantId, memberId, request);
 
         return ApiResponse.success("댓글 작성에 성공했습니다.", response);
     }
 
     // 댓글 수정
     @PatchMapping("/{commentId}")
-    public ApiResponse<CommentDto.UpdateResponse> updateComment(
+    public ApiResponse<CommentUpdateResponse> updateComment(
             @PathVariable Long projectId,
             @PathVariable Long applicantId,
             @PathVariable Long commentId,
             @RequestParam(defaultValue = "document") String stage,
-            @RequestBody CommentDto.Request request
+            @RequestBody CommentRequest request
     ) {
         Long memberId = 1L; // 임시 하드코딩
 
-        CommentDto.UpdateResponse response = commentService.updateComment(commentId, memberId, request);
+        CommentUpdateResponse response = commentService.updateComment(commentId, memberId, request);
 
         return ApiResponse.success("댓글 수정에 성공했습니다.", response);
     }
 
     // 댓글 삭제
     @DeleteMapping("/{commentId}")
-    public ApiResponse<Void> deleteComment(
+    public ApiResponse<?> deleteComment(
             @PathVariable Long projectId,
             @PathVariable Long applicantId,
             @PathVariable Long commentId,
