1c7a0d0ac9f17c239a75537bc20861bc9cbae2d2
jaewon <jaewon0077@naver.com>
2026-01-21 15:00:18 +0900

    [fix] import완료

--- src/main/java/com/campusform/server/identity/presentation/UserController.java
+++ src/main/java/com/campusform/server/identity/presentation/UserController.java
@@ -1,6 +1,8 @@
 package com.campusform.server.identity.presentation;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
