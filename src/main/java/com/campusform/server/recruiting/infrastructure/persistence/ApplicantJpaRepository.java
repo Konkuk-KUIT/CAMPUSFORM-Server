9bba227c680098d6eb737cc7b12bd4491bb56d4c
jaewon <jaewon0077@naver.com>
2026-01-27 21:34:20 +0900

    [fix] 오류 수정, 코멘트내용 수정

--- src/main/java/com/campusform/server/recruiting/infrastructure/persistence/ApplicantJpaRepository.java
+++ src/main/java/com/campusform/server/recruiting/infrastructure/persistence/ApplicantJpaRepository.java
@@ -1,9 +1,12 @@
 package com.campusform.server.recruiting.infrastructure.persistence;
 
+import com.campusform.server.recruiting.domain.model.applicant.value.ApplicantStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.campusform.server.recruiting.domain.model.applicant.Applicant;
+
+import java.util.List;
 
 /**
  * Spring Data JPA를 위한 Applicant Repository
@@ -12,4 +15,10 @@ import com.campusform.server.recruiting.domain.model.applicant.Applicant;
  */
 @Repository
 public interface ApplicantJpaRepository extends JpaRepository<Applicant, Long> {
+    // JPA가 이름만 보고 자동으로 쿼리를 만들어줌.
+    long countByProjectId(Long projectId);
+
+    List<Applicant> findByProjectIdAndDocumentStatus(Long projectId, ApplicantStatus documentStatus);
+
+    List<Applicant> findByProjectIdAndInterviewStatus(Long projectId, ApplicantStatus interviewStatus);
 }
